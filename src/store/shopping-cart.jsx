4f899f090ad26bf2a1739dbe9a1b958b406642c8
Wilton24 <cbasher24@gmail.com>
2025-05-31 17:55:57 +0800

    update item quantity fixed

--- src/store/shopping-cart.jsx
+++ src/store/shopping-cart.jsx
@@ -1,4 +1,4 @@
-import { createContext, useReducer, useState } from "react";
+import { createContext, useReducer } from "react";
 import { DUMMY_PRODUCTS } from "../dummy-products"
 
 
@@ -9,21 +9,12 @@ export const CartContext = createContext({
 });
 
 function shoppingCartReducer(state, action){
-  return state
-};
-
-export default function CartContextProvider({children}) {
-  const [shoppingCartState, shoppingCartDispatch] = useReducer(shoppingCartReducer, {items:[]});
-  const [shoppingCart, setShoppingCart] = useState({
-    items: [],
-  });
-
-  function handleAddItemToCart(id) {
-    setShoppingCart((prevShoppingCart) => {
-      const updatedItems = [...prevShoppingCart.items];
+  switch (action.type){
+    case "ADD_ITEM":
+    const updatedItems = [...state.items];
 
       const existingCartItemIndex = updatedItems.findIndex(
-        (cartItem) => cartItem.id === id
+        (cartItem) => cartItem.id === action.payload
       );
       const existingCartItem = updatedItems[existingCartItemIndex];
 
@@ -34,9 +25,9 @@ export default function CartContextProvider({children}) {
         };
         updatedItems[existingCartItemIndex] = updatedItem;
       } else {
-        const product = DUMMY_PRODUCTS.find((product) => product.id === id);
+        const product = DUMMY_PRODUCTS.find((product) => product.id === action.payload);
         updatedItems.push({
-          id: id,
+          id: action.payload,
           name: product.title,
           price: product.price,
           quantity: 1,
@@ -44,34 +35,67 @@ export default function CartContextProvider({children}) {
       }
 
       return {
+        ...state,
         items: updatedItems,
       };
-    });
-  }
+      break;
+      // UPDATE_ITEM_QUANTITY -----------------------------------------------------
+    case "UPDATE_ITEM_QUANTITY":
+      const cartItems = [...state.items];
 
-  function handleUpdateCartItemQuantity(productId, amount) {
-    setShoppingCart((prevShoppingCart) => {
-      const updatedItems = [...prevShoppingCart.items];
-      const updatedItemIndex = updatedItems.findIndex(
-        (item) => item.id === productId
-      );
+      const selectedItemIndex = cartItems.findIndex(item => item.id === action.payload.productId);            
+      const selectedItem = cartItems[selectedItemIndex];      
 
-      const updatedItem = {
-        ...updatedItems[updatedItemIndex],
-      };
-
-      updatedItem.quantity += amount;
-
-      if (updatedItem.quantity <= 0) {
-        updatedItems.splice(updatedItemIndex, 1);
-      } else {
-        updatedItems[updatedItemIndex] = updatedItem;
+      console.log("Selected Item quantity :", selectedItem.quantity);
+      
+      if(selectedItem.quantity <= 0){
+        cartItems.splice(selectedItemIndex, 1);
+      } else{
+        selectedItem.quantity += action.payload.amount;
       }
 
       return {
-        items: updatedItems,
+        ...state,
+        items: cartItems
       };
-    });
+
+      // const updatedItem = {
+      //   ...cartItems[updatedItemIndex],
+      // };
+
+      // updatedItem.quantity += parseInt(action.payload.amount);
+
+      // if (updatedItem.quantity <= 0) {
+      //   cartItems.splice(updatedItemIndex, 1);
+      // } else {
+      //   cartItems[updatedItemIndex] = updatedItem;
+      // }
+
+      // return {
+      //   ...state,
+      //   items: cartItems,
+      // };
+  }
+};
+
+export default function CartContextProvider({children}) {
+  const [shoppingCartState, shoppingCartDispatch] = useReducer(shoppingCartReducer, {items:[]});
+
+  function handleAddItemToCart(id) {
+    shoppingCartDispatch({
+      type: 'ADD_ITEM',
+      payload: id
+    })
+  };
+
+  function handleUpdateCartItemQuantity(productId, amount) {
+    shoppingCartDispatch({
+      type: "UPDATE_ITEM_QUANTITY",
+      payload: {
+        productId,
+        amount
+      }
+    })
   }
 
   const ctxValue = {
